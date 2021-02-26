3f6b150621314342f1bd1ac9ef8e727c5e301a4d
slandery <75763392+slandery@users.noreply.github.com>
2021-02-25 18:53:10 -0600

    first commit

---
+++ src/main/java/com/tts/validatingform/ValidatingformApplication.java
@@ -0,0 +1,13 @@
+package com.tts.validatingform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingformApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ValidatingformApplication.class, args);
+	}
+
+}
